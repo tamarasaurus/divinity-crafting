fe57a2f1823f52f62877fcfe2b6e22cb5c186981
Tamara Chahine <chahine.tamara@abc.net.au>
2014-08-24 10:54:19 +1000

    Update package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,47 @@ var gulp = require('gulp');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var jade = require('gulp-jade');
-var jadeify = require('browserify-jade').jade({ pretty: false })
-var source = require('vinyl-source-stream')
+var jadeify = require('browserify-jade').jade({
+	pretty: false
+});
+var source = require('vinyl-source-stream');
 
-var handleError = function(e){
+var handleError = function(e) {
 	console.log(e);
-}
+};
 
-var compile_jade = function(){
+var compile_jade = function() {
 	gulp.src('./src/index.jade')
 		.pipe(jade())
-		.pipe(gulp.dest('./build'))
-}
+		.pipe(gulp.dest('./build'));
+};
 
-var compile_scripts = function(){
-	var file = './src/js/app.js'
-	var opts = { extensions: [ '.jade', '.json' ] }
+var compile_scripts = function() {
+	var file = './src/js/app.js';
+	var opts = {
+		extensions: ['.jade', '.json']
+	};
 
 	var stream = browserify(file, opts)
-	    .transform(jadeify)
-	    .bundle()
-	    .on('error', handleError)
-	    .pipe(source('index.js'))
-	    .on('end', function() {
-	    })
+		.transform(jadeify)
+		.bundle()
+		.on('error', handleError)
+		.pipe(source('index.js'))
+		.on('end', function() {});
 	stream.pipe(gulp.dest('./build/'));
-}
+};
 
-gulp.task('build', function (){
-    compile_scripts();
-    compile_jade();
+gulp.task('build', function() {
+	compile_scripts();
+	compile_jade();
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/js/**/*.jade', ['build']);
-    gulp.watch('src/js/**/*.js', ['build']);
+gulp.task('watch', function() {
+	var watcher = gulp.watch('./src/**/*', ['build']);
+	watcher.on('change', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
 });
+
 
 gulp.task('default', ['build', 'watch']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,6 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
 var data = require('./data');
-console.log(data);
+
+console.log(Backbone, data);
