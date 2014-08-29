07ffd21def1c3693b1fb46547e4fba43824e3bcb
Tamara Chahine <chahine.tamara@abc.net.au>
2014-08-29 23:02:33 +1000

    #Fix #14 Add expand/collapse for cards

--- build/index.js
+++ build/index.js
@@ -10054,12 +10054,19 @@ var template = require('../templates/combinations.jade');
 module.exports = View.extend({
 	tagName: 'ul',
 	className: 'item-combo-list',
+	events: {
+		'click .arrow': 'toggle'
+	},
 
 	initialize: function() {
 		this.collection.on('reset', this.render, this);
 		this.render();
 	},
 
+	toggle: function(e) {
+		console.log(e, $(e.currentTarget).parent());
+		$(e.currentTarget).parent().toggleClass('active');
+	},
 	render: function() {
 		this.$el.html(template({
 			items: this.collection.models,
